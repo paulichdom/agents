ab288a40b54864c527de1bd55591d52adfce8c00
Domagoj <paulic.domagoj@gmail.com>
2025-08-29 10:51:48 +0200

    Add SQL tools for listing and describing database tables

--- main.py
+++ main.py
@@ -4,22 +4,27 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder
 )
+from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 load_dotenv()
 
 chat = ChatOpenAI()
+
+tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(
+            content=f"You are an AI that has access to a SQLite database with the following tables:\n{tables}"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
