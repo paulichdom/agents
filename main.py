c4bff94b307306c6b19bc9f3288de511c5471cce
Domagoj <paulic.domagoj@gmail.com>
2025-08-31 10:40:36 +0200

    add report writing functionality and report generation

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
+from tools.report import write_report_tool
 
 load_dotenv()
 
@@ -29,7 +30,11 @@ prompt = ChatPromptTemplate(
     ]
 )
 
-tools = [run_query_tool, describe_tables_tool]
+tools = [
+    run_query_tool,
+    describe_tables_tool,
+    write_report_tool
+]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
@@ -43,4 +48,5 @@ agent_executor = AgentExecutor(
     tools=tools
 )
 
-agent_executor.run("How many shipping addresses are in the database?")
+agent_executor.run(
+    "Summarize the top 5 most popular products. Write the results to a report file.")
